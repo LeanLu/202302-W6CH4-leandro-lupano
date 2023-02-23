5acfa0c7699b95b1c3dfbedfdea1df058334251a
Leandro <leandrolupano@hotmail.com>
2023-02-23 21:32:53 +0100

    🔧 Add partial calculator

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,61 @@
-/* eslint-disable no-unused-vars */
 import http from 'http';
-import url from 'url';
-import * as dotenv from 'dotenv';
-import { addKnowledge, getAllKnowledge } from './functions.js';
-
-dotenv.config();
+import url, { URLSearchParams } from 'url';
 
 const PORT = process.env.PORT || 3200;
 
 const server = http.createServer((request, response) => {
-  switch (request.method) {
-    case 'GET':
-      response.write('GET');
-      break;
-
-    case 'POST':
-      response.write('Post');
-      break;
-
-    case 'PATCH':
-      response.write('Patch');
-      break;
-
-    case 'DELETE':
-      response.write('Delete');
-      break;
-
-    default:
-      break;
+  if (!request.url) {
+    server.emit('error', new Error('Invalid url'));
+    return;
   }
+
+  if (request.method !== 'GET') {
+    response.write('This method is not available');
+    return;
+  }
+
+  const { pathname, search } = url.parse(request.url);
+
+  if (pathname !== '/calculator') {
+    server.emit('error', new Error('Invalid url'));
+    return;
+  }
+
+  const urlParams = new URLSearchParams(search!);
+
+  const a = Number(urlParams.get('a'));
+  const b = Number(urlParams.get('b'));
+
+  const sum = a + b;
+  const rest = a - b;
+  const multiply = a * b;
+  const div = a / b;
+
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+
+  response.write(
+    `
+    <html>
+            <head>
+              <title>Calculator</title>
+              <link rel="shortcut icon" href="#">
+            </head>
+            <body>
+              <h1>Calculator Result</h1>
+              <p>${a} + ${b} = ${sum}</p>
+              <p>${a} - ${b} = ${rest}</p>
+              <p>${a} * ${b} = ${multiply}</p>
+              <p>${a} / ${b} = ${div}</p>
+            </body>
+          </html>
+    `
+  );
 
   response.end();
 });
 
-server.on('listening', () => console.log(PORT));
+server.on('listening', () =>
+  console.log('Listening in http://localhost: ' + PORT)
+);
 
 server.listen(PORT);
